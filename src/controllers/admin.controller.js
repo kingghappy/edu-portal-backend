5889a442dfcbd92babd52d1ac3d269d936e1a7a9
kinghappy <kalikpance@gmail.com>
2025-06-24 22:30:11 +0700

    done CRUD and init jwt

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -3,38 +3,81 @@ import Teacher from "../models/teacher.js";
 
 // check type
 const models = {
-    'student': Student,
-    'teacher': Teacher
-}
+  student: Student,
+  teacher: Teacher,
+};
 
 const getAll = async (req, res) => {
-    const {type} = req.body
-    const model = models[type]
-    try {
-        const data = await model.find() 
-        res.json({data})
-    } catch (error) {
-        console.log("error: ", error)
-        res.json({error})
-    }
-}
+  const { type } = req.body;
+  const model = models[type];
+  try {
+    const data = await model.find();
+    res.json({ data });
+  } catch (error) {
+    console.log("error: ", error);
+    res.json({ error });
+  }
+};
 
 const importData = async (req, res) => {
-    const {data, type} = req.body
+  const { data, type } = req.body;
 
-    const model = models[type] 
+  const model = models[type];
+  try {
+    // add list of student data to db
+    const result = await model.insertMany(data);
+    console.log("Add data successfull !!");
+    res.json(result);
+  } catch (error) {
+    console.log("Add error", error);
+    res.json({ error });
+  }
+};
+
+const findUser = async (req, res) => {
+  const { type, id } = req.body;
+
+  const model = models[type];
+  try {
+    const user = await model.findById(id);
+    console.log("Get data success !!");
+    res.json({ user });
+  } catch (error) {
+    console.log("error", error);
+    res.json({ error });
+  }
+};
+
+const deleteUser = async (req, res) => {
+    const {type, id} = req.body
+
+    const model = models[type]
     try {
-        // add list of student data to db 
-        const result = await model.insertMany(data)
-        console.log('Add data successfull !!')
-        res.json(result)
+        const deleteUser = await model.findByIdAndDelete(id)
+        console.log('Delete success !!')
+        res.json({deleteUser})
     } catch (error) {
-        console.log('Add error', error)
+        console.log('error', error)
         res.json({error})
     }
 }
 
-export {
-    getAll,
-    importData
-}
+const updateUser = async (req, res) => {
+  const { type, id, data } = req.body;
+  const model = models[type];
+
+  try {
+    const dataNew = await model.findByIdAndUpdate(
+      id,
+      { $set: data },
+      { new: true }
+    );
+    console.log('Update success !!')
+    res.json({dataNew})
+  } catch (error) {
+    console.log('Update error: ', error)
+    res.json({error})
+  }
+};
+
+export { getAll, importData, findUser, updateUser, deleteUser};
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getAll, importData } from '../controllers/admin.controller.js'
+import { getAll, importData, findUser,updateUser,deleteUser } from '../controllers/admin.controller.js'
 
 const router = express.Router()
 
 router.get('/all', getAll)
+router.get('/find', findUser)
 router.post('/add', importData)
+router.post('/update', updateUser)
+router.post('/delete', deleteUser)
+
 
 export default router
